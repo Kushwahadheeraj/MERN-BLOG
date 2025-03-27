2526c49c40fc32ba025975039f84aeb1edbb80b4
Kushwahadheeraj <kushwahadheeraj245@gmail.com>
2025-03-27 13:42:18 +0530

    add pre installation

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -6,10 +6,10 @@ import react from '@vitejs/plugin-react'
     server: {
       proxy: {
         '/api': {
-          target: 'http://localhost:3005',
+          target: 'https://blog-backend-x2i5.onrender.com',
           secure: false,
         },
       },
-    },
+    }, 
   plugins: [react()],
 })
